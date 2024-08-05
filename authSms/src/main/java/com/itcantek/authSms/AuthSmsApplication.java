d80eb500b3ce57a81d4b2589f5b40aa1715b7b00
diafonde <diamoussa@gmail.com>
2024-08-05 11:50:21 +0000

    first commit

---
+++ authSms/src/main/java/com/itcantek/authSms/AuthSmsApplication.java
@@ -0,0 +1,13 @@
+package com.itcantek.authSms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSmsApplication.class, args);
+	}
+
+}
---
+++ authSms/src/test/java/com/itcantek/authSms/AuthSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcantek.authSms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
